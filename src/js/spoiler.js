d3564328fc01c5bb86360687e2a8f6913fbbe253
haranycheva <mimimashkabumazka@gmail.com>
2023-08-27 23:20:34 +0300

    Update spoiler.js

--- src/js/spoiler.js
+++ src/js/spoiler.js
@@ -3,19 +3,23 @@ import { refs } from './refs.js';
 export function questionClick(e) {
   const target = e.currentTarget;
   refs.questions.forEach(e => {
-    if(e.classList.contains("active-js")){
-        e.classList.remove("active-js")
-        e.nextElementSibling.classList.remove('visible-js');
+    if (e.classList.contains('active-js')) {
+      e.classList.remove('active-js');
+      e.nextElementSibling.classList.remove('visible-js');
     }
-  })
+  });
   target.classList.toggle('active-js');
   target.nextElementSibling.classList.toggle('visible-js');
   const ul = target.closest('ul');
-  if(refs.questions.some(e => {
-    return e.classList.contains("active-js")
-  })){
-    ul.classList.contains("active-question-js") ? "" : ul.classList.add("active-question-js")
+  if (
+    refs.questions.some(e => {
+      return e.classList.contains('active-js');
+    })
+  ) {
+    ul.classList.contains('active-question-js')
+      ? ''
+      : ul.classList.add('active-question-js');
   } else {
-    ul.classList.remove("active-question-js")
+    ul.classList.remove('active-question-js');
   }
 }
